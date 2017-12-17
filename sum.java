d9a0bc96d416d202a2cd536a93ee1e48f206b021
AshokDevops2 <gashokumar410@gmail.com>
2017-12-17 19:34:52 +0530

    sum

---
+++ sum.java
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n  Enter A , B and C Vlaues");
+scanf("%d%d%d",&a&b&c);
+c=a+b+c;
+printf("\n The sum of three values are %d",c);
+getch();
+}
